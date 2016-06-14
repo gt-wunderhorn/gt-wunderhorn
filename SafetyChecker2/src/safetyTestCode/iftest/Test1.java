39694828f5c1c304883def9559cab8f47e4be730
burak <buraksahin@gatech.edu>
2016-06-14 15:26:44 -0400

    readme

--- SafetyChecker2/src/safetyTestCode/iftest/Test1.java
+++ SafetyChecker2/src/safetyTestCode/iftest/Test1.java
@@ -5,7 +5,7 @@ import safetyChecker.ErrorLable;
 public class Test1 {
 
 	public void test() {
-		int x = callee();
+		int x = 3; //callee();
 		if(x != 3)
 			ErrorLable.Error();
 	}
--- SafetyChecker2/whiletest1.sh
+++ SafetyChecker2/whiletest1.sh
@@ -4,9 +4,9 @@ echo ">>> clean .dot and .ps files"
 rm *ps
 rm *dot
 echo ">>> Compiling"
-javac -d bin -sourcepath src src/safetyTester/arraytest/Tester1.java -cp ./bin:./lib/* 
+javac -d bin -sourcepath src src/safetyTester/whiletest/Tester1.java -cp ./bin:./lib/* 
 echo ">>> Executing"
-java -cp ./bin:./lib/* safetyTester.arraytest.Tester1
+java -cp ./bin:./lib/* safetyTester.whiletest.Tester1
 echo ">>> creating .ps files"
 python PngCreator.py
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Ended ..."
