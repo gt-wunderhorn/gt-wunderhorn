f0a7e0cb13f5bb0fd2a6b722d08a7bd883c99a66
burak <buraksahin@gatech.edu>
2016-07-26 10:17:47 -0400

    parameters

--- SafetyChecker2/src/safetyTestCode/interproc/Test1.java
+++ SafetyChecker2/src/safetyTestCode/interproc/Test1.java
@@ -5,19 +5,22 @@ import safetyChecker.utilities.ErrorLable;
 public class Test1 {
 
 	public void test(int n) {
-		int j = 2;
-		int k = 3;
+		int j = 1;
+		int k = 5;
 		int i = getNumber(j, k);
-		if(i == 5) 
+		if(i == 15) 
 			ErrorLable.Error();
 
 	}
 
 	public int getNumber(int j, int k){
 			
-		int i = j + k;
+		int i = j + getDouble(k);
 
 		return i;
 	}
+
+
+	int getDouble(int k) { return k*2; }
 
 }
