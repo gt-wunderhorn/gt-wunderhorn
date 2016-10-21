1c7119389d9772a7361a8502dfaeb93057681875
David Heath <heath.davidanthony@gmail.com>
2016-10-21 17:13:07 -0400

    added documentation

--- src/ir_to_graph.ml
+++ src/ir_to_graph.ml
@@ -3,13 +3,21 @@ module G = L.PG
 
 let id_initialized = ref false
 
+(** The global class array which stores the types of objects. *)
 let class_array = ("CLASS_TYPE", L.Array L.Int)
+(** The global identity counter which keeps track of how many objects have been
+    created. *)
 let id = ("ID", L.Int)
 
 let update_arr arr idx e =
   let store = L.ArrStore (L.Var arr, idx, e) in
   L.mk_assign arr store
 
+(** Building a new object has a few steps.
+    1. If the global identity counter does not exist yet, it is created.
+    2. If the counter already existed, it is incremented from its previous value.
+    3. The object's variable is assigned the value of the counter.
+    4. The type of the new object is stored in the global class array. *)
 let build_object v ct =
   let id_init =
     if !id_initialized
@@ -23,20 +31,27 @@ let build_object v ct =
   ; update_arr class_array (L.Var v) ct
   ]
 
+(** To inspect the type of an object, the global class array is accessed
+    and the contents under the object are compared to the expected value. *)
 let check_type obj t =
   L.Call (L.mk_eq (L.ArrSelect (L.Var class_array, obj)) t)
 
-let rec show_sort = function
-  | L.Bool -> "Bool"
-  | L.Int  -> "Int"
-  | L.Real -> "Real"
-  | L.Array s  -> "Array_" ^ show_sort s
-
 let rec instr (this, next, i) =
-  let linear instr = G.singleton (this, next, instr) in
+  (** Many instruction types simply proceed to the next program location with
+      some instructions on the edge. *)
+  let linear instrs = G.singleton (this, next, instrs) in
 
+  (** One program location indicates another with an empty edge. *)
   let jump lbl = G.singleton (this, lbl, []) in
 
+  (** A call has a few important parts:
+        1. An edge is generated which contains the assignments of the formal
+           parameters of the target procedure to the call arguments.
+        2. An edge is generated which assigns the callers assignment variable
+           to the return variable of the callee. It also relates the return
+           site to the call site. (In other words, it distinguishes between
+           the returns of different calls to the same function.)
+        3. A separate graph is evaluated for the callee. *)
   let call pred proc v args =
     let assignments = List.map2 L.mk_assign proc.Ir.params args in
     G.union
@@ -56,12 +71,19 @@ let rec instr (this, next, i) =
     | Ir.Return (d, v, e)       -> G.singleton (this, d, [L.mk_assign v e])
     | Ir.Goto d                 -> jump d
 
+    (** An if statement generates a graph with two edges diverging from one
+        starting node. One edge proceeds to the target destination if the
+        condition is true. The other proceeds to the next location if the
+        condition is false *)
     | Ir.If (cmp, x, y, d) ->
       G.of_list
         [ (this, d, [L.Call (cmp x y)])
         ; (this, next, [L.Call (L.mk_not (cmp x y))])
         ]
 
+    (** A dynamic dispatch generates a graph with many edges diverging from
+        one node. Each divergent edge is predicated by a check to see which
+        version of the dispatch should be used. *)
     | Ir.Dispatch (obj, ps, v, args) ->
       let call_meth (t, proc) =
         call [check_type obj t] proc v (obj :: args) in
